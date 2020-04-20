c11afb5d52416e58af9630fa7a472e74e66d66d9
pippo <schembrigiuseppe@yahoo.it>
2020-04-20 12:28:10 +0200

    Inserted list of chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,5 +4,12 @@ require "tilt/erubis"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
+  @chapters = []
+  file = "data/toc.txt"
+  File.open(file) do |file_content|
+    file_content.each_line do |chapter_title|
+      @chapters << chapter_title
+    end
+  end
   erb :home
 end
