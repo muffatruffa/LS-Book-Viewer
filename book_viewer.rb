53335ddfb93b3d2f7068b10e8dff546d33f76f3b
pippo <schembrigiuseppe@yahoo.it>
2020-04-21 19:56:57 +0200

    Implemented Route Parameters

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,27 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+set :titles, []
+
+file = "data/toc.txt"
+chapter_number = 1
+Title = Struct.new(:number, :text)
+File.open(file) do |file_content|
+  file_content.each_line do |chapter_title|
+    settings.titles << Title.new(chapter_number, chapter_title)
+    chapter_number += 1
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @chapters = []
-  file = "data/toc.txt"
-  File.open(file) do |file_content|
-    file_content.each_line do |chapter_title|
-      @chapters << chapter_title
-    end
-  end
+
   erb :home
 end
 
 get "/chapter/:number" do
   @title = "The Adventures of Sherlock Holmes"
-  @chapters = []
-  file = "data/toc.txt"
-  File.open(file) do |file_content|
-    file_content.each_line do |chapter_title|
-      @chapters << chapter_title
-    end
-  end
+
   @chapert_content = File.read("data/chp#{params[:number]}.txt")
+  @chapter_title = settings.titles[params[:number].to_i - 1].text
   erb :chapter
 end
